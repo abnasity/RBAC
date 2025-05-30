1a4403a7c9e4947016084cb62f1abe6c2eb45709
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-30 16:17:58 +0300

    add admin roles

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,22 @@
 from flask import Blueprint
+from flask_security import login_required, current_user, roles_required
+
+
+
 
 main_bp = Blueprint('main_bp', __name__)
 
 @main_bp.route('/') 
+@login_required
 def home():
-    return "Welcome to the Home Page!"
+    return f"Welcome to the County Portal, {current_user.email}!"
 
 @main_bp.route('/about')
 def about():
     return "This is the county portal, providing information about local governance and services."
+
+
+@main_bp.route('/dashboard')
+@roles_required('super_admin')  # Only users with the 'super_admin' role can access this route
+def dashboard():
+    return "This dashboard is accessible to admins only."
