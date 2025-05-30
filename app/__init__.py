4038bb63feb19a1b82a731169e019add06f08786
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-30 13:56:53 +0300

    initialize flask security

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
-from app.extensions import db, migrate
+from flask_security import SQLAlchemyUserDatastore
+from app.extensions import db, migrate, mail, security
 
 from config import Config
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
+    mail.init_app(app)
+   
+    
     
     #Register blueprints or routes here
     #Blueprints imports
@@ -17,6 +22,14 @@ def create_app():
     #Register the blueprint
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp)
+    
+    #import models and initialize flask_security
+    from app.models.user import User, Role
+    
+    
+   # Setup Flask-Security
+    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    security.init_app(app, user_datastore)
     
     with app.app_context():
         db.create_all()
