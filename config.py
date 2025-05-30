8ffcad252124bc1035ec30190514de3518af1680
Kipngeno Abednego <abnasgit@gmail.com>
2025-05-30 14:51:27 +0300

    add security password salt

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///county.db')
     SQLALCHEMY__TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SECRET_PASSORD_SALT = os.getenv('SECRET_PASSWORD_SALT')
+    SECRET_PASSWORD_SALT = os.getenv('SECRET_PASSWORD_SALT')
     
     #Flask-security settings
     SECURITY_REGISTERABLE = True
     SECURITY_SEND_REGISTER_EMAIL = False
     SECURITY_PASSWORD_HASH = "bcrypt" #use bcrypt for password hashing over argon2
-    
+    SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
